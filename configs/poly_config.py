8dfda64eb2a016e4ff973a82fd9afacdddb6c02f
Max <ooley666@gmail.com>
2025-08-04 12:46:35 +0100

    Updated gitignore

---
+++ configs/poly_config.py
@@ -0,0 +1,11 @@
+events_endpoint: str = "https://gamma-api.polymarket.com/events"
+markets_endpoint: str = "https://gamma-api.polymarket.com/markets"
+
+websocket_endpoint = "wss://ws-subscriptions-clob.polymarket.com/ws/market"
+
+overview_event_fields = ["id", "title", "description", "endDate", "volume"]
+favourite_tags_slug = ["ukraine", "geopolitics", "open-source-intel", "pop-culture"]
+
+
+host: str = "https://clob.polymarket.com"
+chain_id: int = 137
